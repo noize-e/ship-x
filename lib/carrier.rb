a8a69e65a8d685a9046b117fb4c80992045a6a60
fco.gc <me@local>
2021-01-17 05:54:02 -0600

    modified: file indentation spaces

--- lib/carrier.rb
+++ lib/carrier.rb
@@ -1,32 +1,32 @@
 module Carrier
-    STATUS = ['CREATED',
-              'ON_TRANSIT',
-              'DELIVERED',
-              'EXCEPTION']
+  STATUS = ['CREATED',
+    'ON_TRANSIT',
+    'DELIVERED',
+    'EXCEPTION']
 
-    @@carriers = {}
+  @@carriers = {}
 
-    def self.register class_name, &class_ref
-        @@carriers[class_name] = Class.new do
-            instance_eval(&class_ref)
-        end
+  def self.register class_name, &class_ref
+    @@carriers[class_name] = Class.new do
+      instance_eval(&class_ref)
+    end
+  end
+
+  def self.connect carrier_name, config
+    @@config = config
+    @@carrier = @@carriers[carrier_name].connect(config['auth'])
+  end
+
+  def self.track(code)
+    result = @@carrier.track(code)
+    codes = @@config['shipping']['status_codes']
+
+    status = STATUS[codes[result[:status_code]]]
+
+    if not result[:exception].nil?
+      status = STATUS[3]
     end
 
-    def self.connect carrier_name, config
-        @@config = config
-        @@carrier = @@carriers[carrier_name].connect(config['auth'])
-    end
-
-    def self.track(code)
-        result = @@carrier.track(code)
-        codes = @@config['shipping']['status_codes']
-
-        status = STATUS[codes[result[:status_code]]]
-
-        if not result[:exception].nil?
-            status = STATUS[3]
-        end
-
-        {:status => status}
-    end
+    {:status => status}
+  end
 end
--- lib/shipping.rb
+++ lib/shipping.rb
@@ -4,31 +4,31 @@ require 'yaml'
 
 
 module Shipment
-    def self.track(carrier, code)
-        command = Command.new
+  def self.track(carrier, code)
+    command = Command.new
 
-        # load carrier settings
-        command.add params: {:carrier_name => carrier} do |params|
-            # load custom carrier settings from yaml file
-            config_path = "./config/carriers/#{params[:carrier_name]}.yml"
-            {:config => YAML.load(File.open(config_path).read)}
-        end
-
-        # Register and connect to carrier's api
-        command.add do |params|
-            require "./lib/carriers/#{params[:carrier_name]}"
-            # connect to carrier's api
-            Carrier.connect(params[:carrier_name], params[:config])
-        end
-
-        # Track shipment
-        command.add params: {:code => code} do |params|
-            # Track the shipment with a tracking number
-            puts Carrier.track(params[:code])
-        end
-
-        command
+    # load carrier settings
+    command.add params: {:carrier_name => carrier} do |params|
+      # load custom carrier settings from yaml file
+      config_path = "./config/carriers/#{params[:carrier_name]}.yml"
+      {:config => YAML.load(File.open(config_path).read)}
     end
+
+    # Register and connect to carrier's api
+    command.add do |params|
+      require "./lib/carriers/#{params[:carrier_name]}"
+      # connect to carrier's api
+      Carrier.connect(params[:carrier_name], params[:config])
+    end
+
+    # Track shipment
+    command.add params: {:code => code} do |params|
+      # Track the shipment with a tracking number
+      puts Carrier.track(params[:code])
+    end
+
+    command
+  end
 end
 
 
