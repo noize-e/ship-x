b058b49489d75d61d0bfdd0caa8a9235b364327a
fnk <wirdlog@gmail.com>
2021-01-17 02:59:49 -0600

    updated: custom carrier class regisgration

--- lib/carriers/fedex.rb
+++ lib/carriers/fedex.rb
@@ -1,10 +1,21 @@
 require './lib/carrier'
+require 'fedex'
 
 
-class Fedex
-    include Carrier
+Carrier.register 'fedex' do
+    def connect config
+        auth = config['auth']
+        @fedex = Fedex::Shipment.new(
+            :key => auth['key'],
+            :password => auth['password'],
+            :account_number => auth['account_number'],
+            :meter => auth['meter'],
+            :mode => auth['mode'])
+        return self
+    end
 
-    def initialize(config)
-        @config = config
+    def track code
+        results = @fedex.track(:tracking_number => code)
+        results.first
     end
 end
