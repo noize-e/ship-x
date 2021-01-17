b85b9c3f2326df10964ab4cc0411a4700dc390c1
fnk <wirdlog@gmail.com>
2021-01-17 04:20:57 -0600

    update: Carrier.track returns the homologated status

--- lib/carrier.rb
+++ lib/carrier.rb
@@ -12,7 +12,21 @@ module Carrier
         end
     end
 
-    def self.connect carrier, config
-        @@carriers[carrier].connect(config)
+    def self.connect carrier_name, config
+        @@config = config
+        @@carrier = @@carriers[carrier_name].connect(config['auth'])
+    end
+
+    def self.track(code)
+        result = @@carrier.track(code)
+        codes = @@config['shipping']['status_codes']
+
+        status = STATUS[codes[result[:status_code]]]
+
+        if not result[:exception].nil?
+            status = STATUS[3]
+        end
+
+        {:status => status}
     end
 end
